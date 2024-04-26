3854fb440b76a34fbfc0d89bf5449c44f48c7ec4
charleysmithschofield <charleysmithschofield@gmail.com>
2024-04-25 20:58:13 -0700

    remove polls seed to make further changes

--- db/seeds/polls.sql
+++
@@ -1,5 +0,0 @@
--- Polls table seeds here
-
--- Example of an insert for polls (Poll: What would you like to do on Friday?) - modify with actual links
-INSERT INTO polls (email, admin_link, share_link, title, option_1, option_2, option_3, option_4)
-VALUES ('adventureawaits@gmail.com', 'example_admin_link_to_go_here', 'example_share_link_to_go_here', 'What would you like to do on Friday', 'Go to the Movies', 'Go to the Beach', 'Go to a Concert', 'Go to the Amusement Park');
