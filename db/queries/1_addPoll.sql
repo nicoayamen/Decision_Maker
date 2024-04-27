dac18de18860bae473aa49ce145a07ae47da2de3
charleysmithschofield <charleysmithschofield@gmail.com>
2024-04-26 20:57:14 -0700

    add addPoll query

---
+++ db/queries/1_addPoll.sql
@@ -0,0 +1,7 @@
+-- Query: Add Poll
+
+-- This query will be used in the addPoll helper function.
+-- The addPoll function will insert the polls details from the 'Make a Poll' page into the database.
+
+INSERT INTO polls (poll_id, email, admin_link, share_link, title, option_1, option_2, option_3, option_4)
+VALUES ('adventureawaits@gmail.com', 'example_admin_link_to_go_here', 'example_share_link_to_go_here', 'What would you like to do on Friday?', 'Friday would be so much better if we spent it together!', 'Go to the Movies', 'Go to the Beach', 'Go to a Concert', 'Go to the Amusement Park');
