53e850a112552026f7876ea3ca502d8499e2b68c
nicoayamen <nicolas.ayala4@gmail.com>
2024-04-28 19:33:48 -0600

    add/edit style for pages

--- routes/pages.js
+++ routes/pages.js
@@ -3,8 +3,15 @@ const router  = express.Router();
 
 // shows a confirmation when user submits decisions
 router.get('/confirm', (req, res) => {
-  console.log(`Route here`);
   res.render('index_confirmation_page');
-}); 
+});
+
+router.get('/vote', (req, res) => {
+  res.render('vote');
+});
+
+router.get('/wait', (req, res) => {
+  res.render('wait');
+});
 
 module.exports = router;
