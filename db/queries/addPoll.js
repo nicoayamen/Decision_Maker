7afb48d4121b1b63309790974b4e8611535426ae
charleysmithschofield <charleysmithschofield@gmail.com>
2024-04-28 18:24:37 -0700

    add auto incrementing poll_id

--- db/queries/addPoll.js
+++ db/queries/addPoll.js
@@ -4,7 +4,6 @@ const db = require('../connection');
 // addPoll helper function
 const addPoll = (pollDetails) => {
   const {
-    poll_id,
     email,
     admin_link,
     share_link,
@@ -18,12 +17,12 @@ const addPoll = (pollDetails) => {
 
   // addPoll SQL query insert into the 'polls' table
   const addPoll = `
-    INSERT INTO polls (poll_id, email, admin_link, share_link, title, description, option_1, option_2, option_3, option_4)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    INSERT INTO polls (email, admin_link, share_link, title, description, option_1, option_2, option_3, option_4)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING *`;
 
   // parameters for the SQL query
-  const values = [poll_id, email, admin_link, share_link, title, description, option_1, option_2, option_3, option_4];
+  const values = [email, admin_link, share_link, title, description, option_1, option_2, option_3, option_4];
 
   return db.query(addPoll, values)
     .then(data => {
