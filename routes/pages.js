6de5ab017f4b9d73745b3b8212781a3795b7ebfb
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-01 21:46:20 -0700

    addPoll corrected in pages

--- routes/pages.js
+++ routes/pages.js
@@ -64,6 +64,15 @@ router.post('/confirm', (req, res) => {
 
   const { title, option_1, option_2, option_3, option_4, email } = req.body;
 
+  addPoll({ title, option_1, option_2, option_3, option_4, email })
+    .then((result) => {
+      res.render("index_confirmation_page");
+    })
+    .catch((error) => {
+      console.error("Error submitting form:", error);
+      res.status(500).send("Error submitting form: " + error.message);
+    });
+
   sendEmail(email)
     .then(msg => console.log(msg))
     .catch(err => console.log(err));
@@ -88,19 +97,5 @@ router.get(`/admin/${}`, (req, res) => {
 });
 
 */
-
-router.post("/confirm", (req, res) => {
-  const { title, description, option_1, option_2, option_3, option_4, email } =
-    req.body;
-
-  addPoll({ title, description, option_1, option_2, option_3, option_4, email })
-    .then((result) => {
-      res.render("index_confirmation_page");
-    })
-    .catch((error) => {
-      console.error("Error submitting form:", error);
-      res.status(500).send("Error submitting form: " + error.message);
-    });
-});
 
 module.exports = router;
