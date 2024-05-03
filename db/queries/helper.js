48fb551820c9a31932e70ffa7acb5b052cc222ee
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-02 19:35:21 -0600

    change sendEmail to take in new arg and poll_id

--- db/queries/helper.js
+++ db/queries/helper.js
@@ -70,9 +70,9 @@ getPoll(3)
 function sendEmail(to, poll_id) {
   return mg.messages.create(process.env.DOMAIN, {
     from: `Decision Maker <${process.env.SENDER_EMAIL}>`,
-    to: Array.isArray(to) ? to : [to], // Ensure to is an array
+    to: Array.isArray(to) ? to : [to],
     subject: "A Decision has Been Made",
-    html: "<h1>A Vote is Needed!</h1><p>Send this email to your friends <a href=`http://localhost:8080/vote/:poll_id`>Share Link</a></p><p>Once they have all voted, you can see the results here <a href='http://localhost:8080/admin/:poll_id'>Admin Link</a></p>"
+    html: `<h1>A Vote is Needed!</h1><p>Send this email to your friends <a href="http://localhost:8080/vote/${poll_id}">Share Link</a></p><p>Once they have all voted, you can see the results here <a href="http://localhost:8080/admin/${poll_id}">Admin Link</a></p>`
   });
 }
 
